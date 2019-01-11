7269481b5abcdff640ea5db8637c192d796d6c8f
James Beattie <jamesbeattie@farmdrop.co.uk>
2019-01-11 16:38:58 +0000

    First commit

---
+++ LeakSpec/LeakSpec.h
@@ -0,0 +1,19 @@
+//
+//  LeakSpec.h
+//  LeakSpec
+//
+//  Created by James Beattie on 11/01/2019.
+//  Copyright © 2019 Farmdrop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LeakSpec.
+FOUNDATION_EXPORT double LeakSpecVersionNumber;
+
+//! Project version string for LeakSpec.
+FOUNDATION_EXPORT const unsigned char LeakSpecVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeakSpec/PublicHeader.h>
+
+
